ac59796ad752f8a967eb6bc414de3d373065f3aa
chang sun <samchangsun@yahoo.com>
2023-03-31 22:02:17 +0000

    dbt_python_2

---
+++ models/raw_stage/dbt_python.py
@@ -0,0 +1,18 @@
+import snowflake.snowpark.functions as f
+from
+    snowflake.snowpark.functions import col
+
+    def model(dbt,session):
+
+    dbt.configure = (materializeview = "table")
+
+# Create a DataFrame from the data in the "sample_product_data" table.
+
+    df_table = dbt.ref("raw_inventory")
+    df_table2 = dbt.ref("raw_orders")
+
+# To print out the first 10 rows, call df_table.show()
+
+    df_result = df_table2.show()
+
+    return df_rsult
